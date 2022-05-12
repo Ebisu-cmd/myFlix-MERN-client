427330dfefab4a12b55489c14b6e674de61e4ec9
Gabriel Jardio <gabejardio@gmail.com>
2022-05-12 18:44:26 -0500

    Create MainView, MovieCard, and MovieView components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import MainView from './components/main-view/main-view'; //imports our main view
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,9 +9,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
@@ -19,4 +18,5 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+const root = createRoot(container);
+root.render(React.createElement(MyFlixApplication))
